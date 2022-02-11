f2128dbdba76ff13b327af7f9c4935d82b58b2fe
Henk ter Harmsel <henkterharmsel92@gmail.com>
2022-02-11 20:45:12 +0100

    Added prod contracts and correct github action

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,9 +1,10 @@
 export default {
-
-    astrobirdz:{
-        97:'0x9624cd2e91504692e120802e80a313f84847dc40'
-    },
-    astroStake: {
-        97: '0x49ae07126cD7fDC1e4be57BDC9Eee6065ecee5E6'
-    },
-}
+  astrobirdz: {
+    56: "0x7f3E9bdB55a0fA72BD6025C0ee1dfC3276cE3CF9",
+    97: "0x9624cd2e91504692e120802e80a313f84847dc40",
+  },
+  astroStake: {
+    56: "0x26acfbF060d2Ca49F50d7793b22633fd8DFa28E5",
+    97: "0x49ae07126cD7fDC1e4be57BDC9Eee6065ecee5E6",
+  },
+};
